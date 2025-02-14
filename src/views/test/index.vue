60940a573ad0cd8d9775b53642e4fa7da83cb521
wyd <2446820366@qq.com>
2025-02-14 14:40:06 +0800

    Update changes 1.1

--- src/views/test/index.vue
+++ src/views/test/index.vue
@@ -18,13 +18,27 @@ import { onMounted } from 'vue'
 import asyncCom from '@/components/test/async-com.vue'
 import asyncLoading from '@/components/test/async-loading.vue'
 
+
+import mainWorker from '/src/utils/worker.ts?worker'
+const worker = new mainWorker()
+// 也可以写成这样
+// import workerURL from '/src/utils/worker.ts?url'
+// const worker = new Worker(workerURL)
 onMounted(() => {
-  const modules = import.meta.glob('/src/utils/*.ts') as Record<string, () => Promise<{ default: () => void }>>
+  const modules = import.meta.glob(['/src/utils/*.ts', '!**/worker.ts']) as Record<
+    string,
+    () => Promise<{ default: () => void }>
+  >
   for (const path in modules) {
     modules[path]().then(mod => {
       console.log(path, mod)
       mod.default()
     })
+  }
+  console.log(worker)
+  worker.postMessage('Hello Worker')
+  worker.onmessage = function (event) {
+    console.log('Message from worker:', event.data)
   }
 })
 </script>
